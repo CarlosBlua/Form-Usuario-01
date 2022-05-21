b30e02c692b88b9e2717a7b93a7cd10b79b67218
Blua Carlos Santiago <BLUAC@tasa.telefonica.com.ar>
2022-05-21 02:19:32 -0300

    corregi el script de lectura de variables

--- js/validation.js
+++ js/validation.js
@@ -1,2 +1,5 @@
-let example = document.getElementById(username);
-console.log(example);
+let user = document.getElementById("username").value;
+console.log(user);
+
+let pass = document.getElementById("password").value;
+console.log(pass);
